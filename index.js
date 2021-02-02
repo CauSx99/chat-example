18c5a8831c498efff3ada79314996fe283801393
CauSx99 <41835751+CauSx99@users.noreply.github.com>
2021-02-02 20:25:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
+  
+   socket.on('username', user => {
+    io.emit('username', user);
+  });
 });
 
 http.listen(port, () => {
